268fa7b4f514b29c5ac5e5d0c52da7351da464cc
ChoiJaeEun <poungki0823@gmail.com>
2021-12-26 13:36:49 +0900

    JpaRepository 를 상속받았기 때문에 @Repository annotation 이 없어도 IoC 된다!!

---
+++ src/main/java/com/example/springsecurityv1/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.example.springsecurityv1.repository;
+
+import com.example.springsecurityv1.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+// CRUD
+// query method
+// @Repository annotation 없어도 IoC 된다! JpaRepository 를 상속받았기 때문
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+}
