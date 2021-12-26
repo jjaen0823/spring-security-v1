8d6ae13e2d7be12230cba8c057de5db78541632e
ChoiJaeEun <poungki0823@gmail.com>
2021-12-26 14:44:26 +0900

    userEntity 를 UserDetails 를 implements 한 PrincipalDetails 객체에 담아 return

--- src/main/java/com/example/springsecurityv1/config/auth/PrincipalDetailsService.java
+++ src/main/java/com/example/springsecurityv1/config/auth/PrincipalDetailsService.java
@@ -1,5 +1,8 @@
 package com.example.springsecurityv1.config.auth;
 
+import com.example.springsecurityv1.model.User;
+import com.example.springsecurityv1.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -11,10 +14,22 @@ import org.springframework.stereotype.Service;
 @Service  // IoC 등록
 public class PrincipalDetailsService implements UserDetailsService {
 
+    private final UserRepository userRepository;
+
+    @Autowired  // Autowired annotation 을 사용하지 않아도 자동 주입 됨!
+    public PrincipalDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         // parameter 로 username 을 사용하려면 input name 이 username 으로 동일해야 함.
         // input user name 변경해서 사용하려면 security config 에서 .usernameParameter("") 로 설정해줘야 함.
+        User userEntity = userRepository.findByUsername(username);
+        if (userEntity != null) {
+            // Security Session [ Authentication [ UserDetails [ User ] ] ]
+            return new PrincipalDetails(userEntity);
+        }
         return null;
     }
 }
