dd79bbb30fb610c4fba8ce9fc65e05d691f78bf6
ChoiJaeEun <poungki0823@gmail.com>
2021-12-31 09:27:33 +0900

    field autowired -> constructor autowired

--- src/main/java/com/example/springsecurityv1/controller/IndexController.java
+++ src/main/java/com/example/springsecurityv1/controller/IndexController.java
@@ -1,12 +1,16 @@
 package com.example.springsecurityv1.controller;
 
+import com.example.springsecurityv1.config.auth.PrincipalDetails;
 import com.example.springsecurityv1.model.User;
 import com.example.springsecurityv1.repository.UserRepository;
-import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,11 +20,46 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller  // return View
 public class IndexController {
 
-    @Autowired
-    private UserRepository userRepository;
+    private final UserRepository userRepository;
+
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
     @Autowired
-    private BCryptPasswordEncoder bCryptPasswordEncoder;
+    public IndexController(UserRepository userRepository, BCryptPasswordEncoder bCryptPasswordEncoder) {
+        this.userRepository = userRepository;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+    }
+
+    // @AuthenticationPrincipal: 해당 annotation 을 통해 session 에 접근할 수 있다.
+    @GetMapping("/test/login")
+    public @ResponseBody String testLogin(Authentication authentication, @AuthenticationPrincipal PrincipalDetails userDetails) {
+        try {
+            PrincipalDetails principalDetails = (PrincipalDetails) authentication.getPrincipal();  // down casting
+            System.out.println("authentication: " + principalDetails.getUser());
+            System.out.println("getUser: " +  userDetails.getUser());
+            System.out.println("attributes: " + userDetails.getAttributes());
+
+            return "check session info";
+        } catch (Exception e) {
+            System.out.println(e);
+            return "ERROR";
+        }
+    }
+    // PrincipalDetails 에서 OAuth2User 를 implements 했기 때문에 PrincipalDetails 객체로 받으면 됨! (자동 casting)
+    @GetMapping("/test/oauth/login")
+    public @ResponseBody String testOAuthLogin(Authentication authentication, @AuthenticationPrincipal PrincipalDetails oAuth2User) {
+         try {
+             OAuth2User oAuth2 = (OAuth2User) authentication.getPrincipal();  // down casting
+             System.out.println("getAuthorities: " + oAuth2.getAuthorities());
+             System.out.println("getUser: " +  oAuth2User.getUser());
+             System.out.println("attributes: " + oAuth2User.getAttributes());
+
+             return "check OAuth info";
+        } catch (Exception e) {
+            System.out.println(e);
+            return "ERROR";
+        }
+    }
 
     @GetMapping({"", "/"})
     public String index(){
@@ -29,8 +68,12 @@ public class IndexController {
         return "index";  // default: index.mustache
     }
 
+    /* @AuthenticationPrincipal 이 활성화되는 시점
+    *
+    * */
     @GetMapping("/user")
-    public @ResponseBody String user() {
+    public @ResponseBody String user(@AuthenticationPrincipal PrincipalDetails principalDetails) {
+        System.out.println(principalDetails.getUser());
         return "user";
     }
 
